4f9c743eebe55f5a636ee630be8f98dbb20a51de
team Eliana Marostica <wprojdb@tempest.wellesley.edu>
2018-05-01 16:27:14 -0400

    Updating createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -3,11 +3,11 @@ use wprojdb_db;
 
 DROP TABLE if exists user;
 CREATE TABLE user(
-	uid int(10) unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    uid int(10) unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
     email varchar(50) NOT NULL,
     name varchar(50) NOT NULL,
-    role set('student', 'client', 'administrator') NOT NULL,
-	hashed char(60) NOT NULL
+    role set('student', 'client', 'administrator') NOT NULL,	
+    hashed char(60) NOT NULL
 ) engine=InnoDB;
 
 DROP TABLE if exists project;
